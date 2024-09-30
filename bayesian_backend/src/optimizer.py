3712153889d86637b39b39c52bcc2268343c802b
sam-harri <sharr125@uottawa.ca>
2024-09-30 12:46:44 -0400

    [fix] added logging.exception and df saving

--- bayesian_backend/src/optimizer.py
+++ bayesian_backend/src/optimizer.py
@@ -1,7 +1,6 @@
-# src/optimizer.py
-
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, Optional
+import time
 
 import optuna
 import polars as pl
@@ -14,63 +13,86 @@ def objective(
     trial: optuna.trial.Trial,
     eng: Any,
     model_name: str,
-    results_df: pl.DataFrame,
-    data_dir: str,
+    trial_data_dir: str,
 ) -> float:
     """
     Objective function for Bayesian optimization using Optuna.
 
     Parameters
     ----------
-    trial (optuna.trial.Trial)
+    trial : optuna.trial.Tria
         Optuna trial object.
     eng : matlab.engine.MatlabEngine
         MATLAB engine instance.
     model_name : str
         Name of the Simulink model.
-    results_df : pl.DataFrame
-        DataFrame to store optimization results.
-    data_dir : str
+    trial_data_dir : str
         Path to the CSV file for storing results.
-
     Returns
     -------
         float: Loss value to minimize.
     """
-    KC = trial.suggest_float("KC", 0.03, 3)
-    KI = trial.suggest_float("KI", 0.0005, 0.5)
+    KC = trial.suggest_float("KC", 0.05, 0.5)
+    KI = trial.suggest_float("KI", 0.005, 0.05)
 
     try:
-        df_simulation = run_simulation(eng, model_name, KC, KI)
+        logging.info(
+            f"Starting Objective for trial {trial.number}, using KC={KC:.4f}, KI={KI:.4f}"
+        )
+        # Time the simulation
+        start_sim_time = time.time()  # Record start time for simulation
+        df_simulation = run_simulation(eng, model_name, KC, KI, trial.number)
+        end_sim_time = time.time()  # Record end time for simulation
+
+        # Calculate and log simulation time in minutes and seconds
+        sim_time_seconds = end_sim_time - start_sim_time
+        sim_minutes, sim_seconds = divmod(sim_time_seconds, 60)
+        logging.info(
+            f"Simulation  for trial {trial.number} completed in {int(sim_minutes)} minutes and {sim_seconds:.2f} seconds"
+        )
+
+        # Time the loss computation
+        start_loss_time = time.time()  # Record start time for loss computation
         loss = compute_loss(df_simulation)
+        end_loss_time = time.time()  # Record end time for loss computation
+
+        # Calculate and log loss computation time in seconds
+        loss_time_seconds = end_loss_time - start_loss_time
+        logging.info(
+            f"Loss function for trial {trial.number} completed in {loss_time_seconds:.2f} seconds"
+        )
 
         # Append results to the DataFrame
         new_row = pl.DataFrame(
             {"Trial": [trial.number], "KC": [KC], "KI": [KI], "Loss": [loss]}
         )
+        results_df = pl.read_csv(f"{trial_data_dir}/optimization_history.csv")
         results_df = pl.concat([results_df, new_row], how="vertical")
 
         # Save the DataFrame to CSV
-        results_df.write_csv(f"{data_dir}/optimization_history.csv")
+        results_df.write_csv(f"{trial_data_dir}/optimization_history.csv")
         logging.info(
             f"Trial {trial.number}: KC={KC:.4f}, KI={KI:.4f}, Loss={loss:.6f}."
         )
 
         # Save individual simulation data
         simulation_filename = (
-            f"{data_dir}/KC-{KC:.4f}_KI-{KI:.4f}_trial-{trial.number}.csv"
+            f"{trial_data_dir}/KC-{KC:.4f}_KI-{KI:.4f}_trial-{trial.number}.csv"
         )
         df_simulation.write_csv(simulation_filename)
-        logging.info(f"Simulation data saved to '{simulation_filename}'.")
 
         return loss
     except Exception as e:
-        logging.error(f"Objective function failed at trial {trial.number}: {e}")
+        logging.exception(f"Objective function failed at trial {trial.number}: {e}")
         return float("inf")
 
 
 def run_optimization(
-    eng: Any, model_name: str, n_trials: int = 50, data_dir: str = "data/"
+    eng: Any,
+    model_name: str,
+    n_trials: int = 50,
+    data_dir: str = "data/",
+    study_name: Optional[str] = None,
 ) -> Dict[str, Any]:
     """
     Runs the Bayesian optimization using Optuna.
@@ -83,22 +105,33 @@ def run_optimization(
         Name of the Simulink model.
     n_trials : int, default=50
         Number of optimization trials.
-    results_csv_path (str):
+    data_dir: str, default='data/'
         Path to the CSV file for storing results.
+    study_name : Optional[str], default=None
+        Name of the Optuna study.
 
     Returns:
         Dict[str, Any]: Dictionary containing the best parameters and loss.
     """
-    # Initialize the results DataFrame
-    results_df = pl.DataFrame(columns=["Trial", "KC", "KI", "Loss"])
+    study = optuna.create_study(direction="minimize", study_name=study_name)
+
+    # Create and save empty results DataFrame
+    results_df = pl.DataFrame(
+        schema={
+            "Trial": pl.Int64,
+            "KC": pl.Float64,
+            "KI": pl.Float64,
+            "Loss": pl.Float64,
+        }
+    )
+    results_df.write_csv(f"{data_dir}/{study_name}/optimization_history.csv")
 
     # Create the study
-    study = optuna.create_study(direction="minimize")
     logging.info("Starting optimization...")
 
     # Define the objective function wrapper
     def wrapped_objective(trial):
-        return objective(trial, eng, model_name, results_df, data_dir)
+        return objective(trial, eng, model_name, f"{data_dir}/{study_name}")
 
     # Run the optimization
     study.optimize(wrapped_objective, n_trials=n_trials)
